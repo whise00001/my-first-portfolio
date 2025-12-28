b30986a743255318dc8d6d2e943ef86fa6a88d79
whise <tujinwei471@gmail.com>
2025-12-28 22:38:05 +0800

    db_user change again

--- app.py
+++ app.py
@@ -4,12 +4,12 @@ from flask import Flask, render_template
 
 load_dotenv()
 app = Flask(__name__)
-
+db_user = os.getenv("USER_ID")
 
 @app.route('/')
 def home():
     # 將 .env 讀取到的 db_user 傳遞給模板，命名為 username
-    db_user = os.getenv("USER_ID")
+   
     return render_template('index.html', username=db_user)
 
 if __name__ == '__main__':
